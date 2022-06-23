26fad2649f3fdd8c9f2eac1d09b6cb222e3e3742
charleshbl <101838845+charleshbl@users.noreply.github.com>
2022-06-22 21:54:03 -0400

    Test d<un dictionary

--- sdk_laboratoire2/LibrairieConversionNote/Conversion.cs
+++ sdk_laboratoire2/LibrairieConversionNote/Conversion.cs
@@ -6,56 +6,81 @@ namespace LibrairieConversionNote
     /// </summary>
     public class Conversion
     {
-        /// <summary>
-        /// Retourne la note du alphanumerique
-        /// </summary>
-        /// <param name="str">La note alphanumerique</param>
-        /// <returns></returns>
-        public string AlphaToNote(string str)
+        //creation d<un dictionary 
+        readonly Dictionary<string, string> alphabeticalToNote = new() {
+        { "A", "LA" },
+        { "B", "SI" },
+        { "C", "DO" },
+        { "D", "RE" },
+        { "E", "MI" },
+        { "F", "FA" },
+        { "G", "SOL" }
+    };
+        public string AlphaToNote(string KEY)
         {
-            //variable de retour
-            string strReturnValue = string.Empty;
-            //On mets toutes les valeurs passées en majuscules
-            switch (str.ToUpperInvariant())
+            while (!alphabeticalToNote.ContainsKey(KEY))
             {
-                case "A":
-                    strReturnValue = "LA";
-                    break;
-                case "B":
-                    strReturnValue = "SI";
-                    break;
-                case "C":
-                    strReturnValue = "DO";
-                    break;
-                case "D":
-                    strReturnValue = "RE";
-                    break;
-                case "E":
-                    strReturnValue = "MI";
-                    break;
-                case "F":
-                    strReturnValue = "FA";
-                    break;
-                case "G":
-                    strReturnValue = "SOL";
-                    break;
-                default: //Valeur n'est pas dans la liste du haut
-                    strReturnValue = "La note n'existe pas";
-                    break;
+                return "Entrée invalide .";
             }
-            //Retourne la valeur
-            return strReturnValue;
+            return alphabeticalToNote[KEY];
         }
-
-        /// <summary>
-        /// Retourne l'alphanumerique de la note
-        /// </summary>
-        /// <param name="str">La note musicale</param>
-        /// <returns></returns>
         public string NoteToAlpha(string str)
         {
-            string strReturnValue = string.Empty;
-            return strReturnValue;
+            while (!alphabeticalToNote.ContainsValue(str))
+            {
+                return "Entrée invalide .";
+            }
+            return alphabetic ;
         }
     }
 }
+
+}
+/// Retourne la note du alphanumerique
+/// </summary>
+/// <param name="str">La note alphanumerique</param>
+/// <returns></returns>
+//        public string AlphaToNote(string str)
+//        {
+//            //variable de retour
+//            string strReturnValue = string.Empty;
+//            //On mets toutes les valeurs passées en majuscules
+//            switch (str.ToUpperInvariant())
+//            {
+//                case "A":
+//                    strReturnValue = "LA";
+//                    break;
+//                case "B":
+//                    strReturnValue = "SI";
+//                    break;
+//                case "C":
+//                    strReturnValue = "DO";
+//                    break;
+//                case "D":
+//                    strReturnValue = "RE";
+//                    break;
+//                case "E":
+//                    strReturnValue = "MI";
+//                    break;
+//                case "F":
+//                    strReturnValue = "FA";
+//                    break;
+//                case "G":
+//                    strReturnValue = "SOL";
+//                    break;
+//                default: //Valeur n'est pas dans la liste du haut
+//                    strReturnValue = "La note n'existe pas";
+//                    break;
+//            }
+//            //Retourne la valeur
+//            return strReturnValue;
+//        }
+
+
+//        /// <summary>
+//        /// Retourne l'alphanumerique de la note
+//        /// </summary>
+//        /// <param name="str">La note musicale</param>
+//        /// <returns></returns>
+
+//}
